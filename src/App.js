a454d6283d4f41b939e6581acbfa209d88a37038
vasanthakumaaran <vasanthakumaaran76@gmail.com>
2022-01-22 23:41:39 +0530

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
 
+import {Home} from './Home';
+import {Department} from './Department';
+import {Employee} from './Employee';
+import {Navigation} from './Navigation';
+
+import {BrowserRouter, Route, Switch,Routes} from 'react-router-dom';
 function App() {
   return (
+    <BrowserRouter>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3 className="m-3 d-flex justify-context-centre">
+         React JS Tutorial
+      </h3>
+       <Navigation/>
+       <switch>
+         <Route path='/' component={Home} exact/>
+         <Route path='/department' component={Department} exact/>
+         <Route path='/employee' component={Employee} exact/>
+       </switch>
     </div>
+    </BrowserRouter>
   );
 }
 
